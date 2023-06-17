21c00e431fd46346219c38cde53cfcc15e63e4b2
fg <mr.dlas25@gmai.com>
2023-06-17 16:28:22 +0300

    Подключена mongo db, реализованы read операции по get зарпосу

--- src/main/java/DlasWEB/dotext/controller/MainController.java
+++ src/main/java/DlasWEB/dotext/controller/MainController.java
@@ -1,24 +1,25 @@
 package DlasWEB.dotext.controller;
 
+import DlasWEB.dotext.model.BlockForMongo;
 import DlasWEB.dotext.model.BlockForMySql;
-import DlasWEB.dotext.model.Views;
+import DlasWEB.dotext.repo.BlockRepoMongoDb;
 import DlasWEB.dotext.repo.BlockRepoMySql;
-import com.fasterxml.jackson.annotation.JsonView;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
     private final BlockRepoMySql blockRepoMySql;
+    private final BlockRepoMongoDb blockRepoMongoDb;
 
     @Autowired
-    public MainController(BlockRepoMySql blockRepoMySql) {
+    public MainController(BlockRepoMySql blockRepoMySql, BlockRepoMongoDb blockRepoMongoDb) {
         this.blockRepoMySql = blockRepoMySql;
+        this.blockRepoMongoDb = blockRepoMongoDb;
     }
 
     @GetMapping
@@ -26,34 +27,66 @@ public class MainController {
         return "Ответ по запросу localhost:8080/api = Привет из REST Api dotext";
     }
 
+    // Get all doc from Mongo
     @GetMapping("text")
-    @JsonView(Views.FullText.class)
-    public List<BlockForMySql> getAllBlocksWithTextFromApi() {
+    public List<BlockForMongo> getAllBlocksWithTextFromApi() {
 
-        return blockRepoMySql.findAll();
+        return blockRepoMongoDb.findAll();
     }
 
+//    // Get all row from MySQL
+//    @GetMapping("text")
+//    @JsonView(Views.FullText.class)
+//    public List<BlockForMySql> getAllBlocksWithTextFromApi() {
+//
+//        return blockRepoMySql.findAll();
+//    }
+
+    //Get one doc from Mongo
     @GetMapping("text/{id}")
-    @JsonView(Views.FullText.class)
-    public BlockForMySql getOneBlockWithTextByIdFromApi(@PathVariable("id") BlockForMySql block) {
+    public BlockForMongo getOneBlockWithTextByIdFromApi(@PathVariable("id") BlockForMongo block) {
         return block;
     }
 
+//    //Get one row from MySQL
+//    @GetMapping("text/{id}")
+//    @JsonView(Views.FullText.class)
+//    public BlockForMySql getOneBlockWithTextByIdFromApi(@PathVariable("id") BlockForMySql block) {
+//        return block;
+//    }
+
+    // Create one new doc in Mongo
     @PostMapping("/text")
-    @JsonView(Views.FullText.class)
-    public BlockForMySql createBlockWithText(@RequestBody BlockForMySql blockForMySql) {
-        blockForMySql.setCreationDate(LocalDateTime.now());
-        return blockRepoMySql.save(blockForMySql);
+    public BlockForMongo createBlockWithText(@RequestBody BlockForMongo blockForMongo) {
+        return blockRepoMongoDb.save(blockForMongo);
     }
 
+    // Create row for Mysql
+//    @PostMapping("/text")
+//    @JsonView(Views.FullText.class)
+//    public BlockForMySql createBlockWithText(@RequestBody BlockForMySql blockForMySql) {
+//        blockForMySql.setCreationDate(LocalDateTime.now());
+//        return blockRepoMySql.save(blockForMySql);
+//    }
+
+    // Update one doc in Mongo
     @PutMapping("text/{id}")
-    public BlockForMySql updateBlockWithText(
-            @PathVariable("id") BlockForMySql blockFromDb,
-            @RequestBody BlockForMySql block
+    public BlockForMongo updateBlockWithText(
+            @PathVariable("id") BlockForMongo blockFromDb
+           , @RequestBody BlockForMongo block
     ) {
-        BeanUtils.copyProperties(block, blockFromDb, "id");
-        return blockRepoMySql.save(blockFromDb);
+            return blockForMongo.setText(block.getText());
     }
+
+//    // Update one row in Mysql
+//    @PutMapping("text/{id}")
+//    public BlockForMySql updateBlockWithText(
+//            @PathVariable("id") BlockForMySql blockFromDb,
+//            @RequestBody BlockForMySql block
+//    ) {
+//        BeanUtils.copyProperties(block, blockFromDb, "id");
+//        return blockRepoMySql.save(blockFromDb);
+//    }
 
     @DeleteMapping("text/{id}")
     public void deleteBlockWithText(
